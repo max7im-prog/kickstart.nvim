70e3c58c0262efe850e6fd6b863295bd5daf2efb
max7im <nogagorilli@gmail.com>
2026-01-02 12:45:43 +0300

    blabla

--- init.lua
+++ init.lua
@@ -241,9 +241,6 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   -- 'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
-  { 'tpope/vim-sleuth' },
-  { 'christoomey/vim-tmux-navigator', lazy = false },
-
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -671,7 +668,7 @@ require('lazy').setup({
         jsonls = {},
         yamlls = {},
         pylsp = {},
-        cmakelang = {},
+        neocmake = {},
         -- jsonls = {
         -- settings = {
         -- json = {
@@ -992,7 +989,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
